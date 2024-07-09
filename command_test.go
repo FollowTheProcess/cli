37527e0e49d1a56983c1d30cca7591da42c734fd
Tom Fleet <me@followtheprocess.codes>
2024-07-09 18:40:51 +0100

    Make the example a proper structure (#2)

--- command_test.go
+++ command_test.go
@@ -31,3 +31,41 @@ func TestExecute(t *testing.T) {
 	want := "Oooh look, it ran, here are some args: [arg1 arg2 arg3]\n"
 	test.Equal(t, stdout.String(), want)
 }
+
+func TestExampleString(t *testing.T) {
+	tests := []struct {
+		name    string
+		example cli.Example
+		want    string
+	}{
+		{
+			name:    "empty",
+			example: cli.Example{},
+			want:    "",
+		},
+		{
+			name:    "only command",
+			example: cli.Example{Command: "run this program --once"},
+			want:    "$ run this program --once",
+		},
+		{
+			name:    "only comment",
+			example: cli.Example{Comment: "Run the program once"},
+			want:    "# Run the program once",
+		},
+		{
+			name: "both",
+			example: cli.Example{
+				Comment: "Run the program once",
+				Command: "run this program --once",
+			},
+			want: "# Run the program once\n$ run this program --once",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			test.Equal(t, tt.example.String(), tt.want)
+		})
+	}
+}
