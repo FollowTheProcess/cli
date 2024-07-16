ad60ce86bd8dfc5eacce0618a89c47833ad66a2c
Tom Fleet <tom.fleet@chetwood.co>
2024-07-16 20:16:50 +0100

    Add a flag Set method

---
+++ flag/flag_test.go
@@ -0,0 +1,182 @@
+package flag_test
+
+import (
+	"testing"
+
+	"github.com/FollowTheProcess/cli/flag"
+	"github.com/FollowTheProcess/test"
+)
+
+func TestFlagValueSet(t *testing.T) {
+	// We can't do table testing here because Flag[T] is different for each test
+	// so strap in for a bunch of copy pasta
+	t.Run("int valid", func(t *testing.T) {
+		var i int
+		intFlag := flag.New(&i, "int", "i", 0, "Set an int value")
+		err := intFlag.Set("42")
+		test.Ok(t, err)
+		test.Equal(t, intFlag.Get(), 42)
+	})
+
+	t.Run("int invalid", func(t *testing.T) {
+		var i int
+		intFlag := flag.New(&i, "int", "i", 0, "Set an int value")
+		err := intFlag.Set("word")
+		test.Err(t, err)
+		test.Equal(t, err.Error(), `flag int received invalid value "word" (expected int)`)
+	})
+
+	t.Run("int8 valid", func(t *testing.T) {
+		var i int8
+		intFlag := flag.New(&i, "int8", "i", 0, "Set an int8 value")
+		err := intFlag.Set("42")
+		test.Ok(t, err)
+		test.Equal(t, intFlag.Get(), int8(42))
+	})
+
+	t.Run("int8 invalid", func(t *testing.T) {
+		var i int8
+		intFlag := flag.New(&i, "int8", "i", 0, "Set an int8 value")
+		err := intFlag.Set("word")
+		test.Err(t, err)
+		test.Equal(t, err.Error(), `flag int8 received invalid value "word" (expected int8)`)
+	})
+
+	t.Run("int16 valid", func(t *testing.T) {
+		var i int16
+		intFlag := flag.New(&i, "int16", "i", 0, "Set an int16 value")
+		err := intFlag.Set("42")
+		test.Ok(t, err)
+		test.Equal(t, intFlag.Get(), int16(42))
+	})
+
+	t.Run("int16 invalid", func(t *testing.T) {
+		var i int16
+		intFlag := flag.New(&i, "int16", "i", 0, "Set an int16 value")
+		err := intFlag.Set("word")
+		test.Err(t, err)
+		test.Equal(t, err.Error(), `flag int16 received invalid value "word" (expected int16)`)
+	})
+
+	t.Run("int32 valid", func(t *testing.T) {
+		var i int32
+		intFlag := flag.New(&i, "int32", "i", 0, "Set an int32 value")
+		err := intFlag.Set("42")
+		test.Ok(t, err)
+		test.Equal(t, intFlag.Get(), int32(42))
+	})
+
+	t.Run("int32 invalid", func(t *testing.T) {
+		var i int32
+		intFlag := flag.New(&i, "int32", "i", 0, "Set an int32 value")
+		err := intFlag.Set("word")
+		test.Err(t, err)
+		test.Equal(t, err.Error(), `flag int32 received invalid value "word" (expected int32)`)
+	})
+
+	t.Run("int64 valid", func(t *testing.T) {
+		var i int64
+		intFlag := flag.New(&i, "int64", "i", 0, "Set an int64 value")
+		err := intFlag.Set("42")
+		test.Ok(t, err)
+		test.Equal(t, intFlag.Get(), int64(42))
+	})
+
+	t.Run("int64 invalid", func(t *testing.T) {
+		var i int64
+		intFlag := flag.New(&i, "int64", "i", 0, "Set an int64 value")
+		err := intFlag.Set("word")
+		test.Err(t, err)
+		test.Equal(t, err.Error(), `flag int64 received invalid value "word" (expected int64)`)
+	})
+
+	t.Run("uint valid", func(t *testing.T) {
+		var i uint
+		intFlag := flag.New(&i, "uint", "i", 0, "Set a uint value")
+		err := intFlag.Set("42")
+		test.Ok(t, err)
+		test.Equal(t, intFlag.Get(), 42)
+	})
+
+	t.Run("uint invalid", func(t *testing.T) {
+		var i uint
+		intFlag := flag.New(&i, "uint", "i", 0, "Set a uint value")
+		err := intFlag.Set("word")
+		test.Err(t, err)
+		test.Equal(t, err.Error(), `flag uint received invalid value "word" (expected uint)`)
+	})
+
+	t.Run("uint8 valid", func(t *testing.T) {
+		var i uint8
+		intFlag := flag.New(&i, "uint8", "i", 0, "Set a uint8 value")
+		err := intFlag.Set("42")
+		test.Ok(t, err)
+		test.Equal(t, intFlag.Get(), uint8(42))
+	})
+
+	t.Run("uint8 invalid", func(t *testing.T) {
+		var i uint8
+		intFlag := flag.New(&i, "uint8", "i", 0, "Set a uint8 value")
+		err := intFlag.Set("word")
+		test.Err(t, err)
+		test.Equal(t, err.Error(), `flag uint8 received invalid value "word" (expected uint8)`)
+	})
+
+	t.Run("uint16 valid", func(t *testing.T) {
+		var i uint16
+		intFlag := flag.New(&i, "uint16", "i", 0, "Set a uint16 value")
+		err := intFlag.Set("42")
+		test.Ok(t, err)
+		test.Equal(t, intFlag.Get(), uint16(42))
+	})
+
+	t.Run("uint16 invalid", func(t *testing.T) {
+		var i uint16
+		intFlag := flag.New(&i, "uint16", "i", 0, "Set a uint16 value")
+		err := intFlag.Set("word")
+		test.Err(t, err)
+		test.Equal(t, err.Error(), `flag uint16 received invalid value "word" (expected uint16)`)
+	})
+
+	t.Run("uint32 valid", func(t *testing.T) {
+		var i uint32
+		intFlag := flag.New(&i, "uint32", "i", 0, "Set a uint32 value")
+		err := intFlag.Set("42")
+		test.Ok(t, err)
+		test.Equal(t, intFlag.Get(), uint32(42))
+	})
+
+	t.Run("uint32 invalid", func(t *testing.T) {
+		var i uint32
+		intFlag := flag.New(&i, "uint32", "i", 0, "Set a uint32 value")
+		err := intFlag.Set("word")
+		test.Err(t, err)
+		test.Equal(t, err.Error(), `flag uint32 received invalid value "word" (expected uint32)`)
+	})
+
+	t.Run("uint64 valid", func(t *testing.T) {
+		var i uint64
+		intFlag := flag.New(&i, "uint64", "i", 0, "Set a uint64 value")
+		err := intFlag.Set("42")
+		test.Ok(t, err)
+		test.Equal(t, intFlag.Get(), uint64(42))
+	})
+
+	t.Run("uint64 invalid", func(t *testing.T) {
+		var i uint64
+		intFlag := flag.New(&i, "uint64", "i", 0, "Set a uint64 value")
+		err := intFlag.Set("word")
+		test.Err(t, err)
+		test.Equal(t, err.Error(), `flag uint64 received invalid value "word" (expected uint64)`)
+	})
+
+	// No invalid case as all command line args are strings anyway so no real way of
+	// getting an error here
+	t.Run("string", func(t *testing.T) {
+		var str string
+		strFlag := flag.New(&str, "string", "s", "", "Set a string value")
+		err := strFlag.Set("newvalue")
+		test.Ok(t, err)
+		test.Equal(t, strFlag.Get(), "newvalue")
+	})
+}
---
+++ flag/parse.go
@@ -0,0 +1,52 @@
+package flag
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// signed is the same as constraints.Signed but we don't have to depend
+// on golang/x/exp.
+type signed interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+// unsigned is the same as constraints.Unsigned but we don't hve to depend
+// on golang/x/exp.
+type unsigned interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+
+// errParse is a helper to quickly return a consistent error in the face of flag
+// value parsing errors.
+func errParse[T any](name, str string, typ T) error {
+	return fmt.Errorf("flag %s received invalid value %q (expected %T)", name, str, typ)
+}
+
+// parseInt is a generic helper to parse all signed integers, given a bit size.
+//
+// It returns the parsed value or an error.
+func parseInt[T signed](bits int) func(str string) (T, error) {
+	return func(str string) (T, error) {
+		val, err := strconv.ParseInt(str, 0, bits)
+		if err != nil {
+			return 0, err
+		}
+
+		return T(val), nil
+	}
+}
+
+// parseUint is a generic helper to parse all signed integers, given a bit size.
+//
+// It returns the parsed value or an error.
+func parseUint[T unsigned](bits int) func(str string) (T, error) {
+	return func(str string) (T, error) {
+		val, err := strconv.ParseUint(str, 0, bits)
+		if err != nil {
+			return 0, err
+		}
+
+		return T(val), nil
+	}
+}
