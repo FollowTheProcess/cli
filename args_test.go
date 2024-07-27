e9e3889157ba4bc427fd669505475c249ef326e9
Tom Fleet <tom.fleet@chetwood.co>
2024-07-27 16:36:09 +0100

    Refactor functional options to act on an intermediate struct (#33)
    * Refactor functional options to act on an intermediate struct
    
    * Better docs

--- args_test.go
+++ args_test.go
@@ -3,6 +3,7 @@ package cli_test
 import (
 	"bytes"
 	"fmt"
+	"slices"
 	"testing"
 
 	"github.com/FollowTheProcess/cli"
@@ -11,215 +12,199 @@ import (
 
 func TestArgValidators(t *testing.T) {
 	tests := []struct {
-		cmd     *cli.Command // The command undkzer test
 		name    string       // Identifier of the test case
 		stdout  string       // Desired output to stdout
 		stderr  string       // Desired output to stderr
 		errMsg  string       // If we wanted an error, what should it say
+		options []cli.Option // Options to apply to the command
 		wantErr bool         // Whether we want an error
 	}{
 		{
 			name: "anyargs",
-			cmd: cli.New(
-				"anyargs",
+			options: []cli.Option{
 				cli.Args([]string{"some", "args", "here"}),
 				cli.Run(func(cmd *cli.Command, args []string) error {
 					fmt.Fprintln(cmd.Stdout(), "Hello from anyargs")
 					return nil
 				}),
 				cli.Allow(cli.AnyArgs),
-			),
+			},
 			wantErr: false,
 			stdout:  "Hello from anyargs\n",
 		},
 		{
 			name: "noargs pass",
-			cmd: cli.New(
-				"noargs",
+			options: []cli.Option{
 				cli.Args([]string{}),
 				cli.Run(func(cmd *cli.Command, args []string) error {
 					fmt.Fprintln(cmd.Stdout(), "Hello from noargs")
 					return nil
 				}),
 				cli.Allow(cli.NoArgs),
-			),
+			},
 			wantErr: false,
 			stdout:  "Hello from noargs\n",
 		},
 		{
 			name: "noargs fail",
-			cmd: cli.New(
-				"noargs",
-				cli.Args([]string{"arg1", "arg2", "arg3"}),
+			options: []cli.Option{
+				cli.Args([]string{"some", "args", "here"}),
 				cli.Run(func(cmd *cli.Command, args []string) error {
 					fmt.Fprintln(cmd.Stdout(), "Hello from noargs")
 					return nil
 				}),
 				cli.Allow(cli.NoArgs),
-			),
+			},
 			wantErr: true,
-			errMsg:  "command noargs accepts no arguments but got [arg1 arg2 arg3]",
+			errMsg:  "command test accepts no arguments but got [some args here]",
 		},
 		{
 			name: "minargs pass",
-			cmd: cli.New(
-				"minargs",
+			options: []cli.Option{
 				cli.Args([]string{"loads", "more", "args", "here"}),
 				cli.Run(func(cmd *cli.Command, args []string) error {
 					fmt.Fprintln(cmd.Stdout(), "Hello from minargs")
 					return nil
 				}),
 				cli.Allow(cli.MinArgs(3)),
-			),
+			},
 			wantErr: false,
 			stdout:  "Hello from minargs\n",
 		},
 		{
 			name: "minargs fail",
-			cmd: cli.New(
-				"minargs",
+			options: []cli.Option{
 				cli.Args([]string{"only", "two"}),
 				cli.Run(func(cmd *cli.Command, args []string) error {
 					fmt.Fprintln(cmd.Stdout(), "Hello from minargs")
 					return nil
 				}),
 				cli.Allow(cli.MinArgs(3)),
-			),
+			},
 			wantErr: true,
-			errMsg:  "command minargs requires at least 3 arguments, but got 2: [only two]",
+			errMsg:  "command test requires at least 3 arguments, but got 2: [only two]",
 		},
 		{
 			name: "maxargs pass",
-			cmd: cli.New(
-				"maxargs",
+			options: []cli.Option{
 				cli.Args([]string{"two", "args"}),
 				cli.Run(func(cmd *cli.Command, args []string) error {
 					fmt.Fprintln(cmd.Stdout(), "Hello from maxargs")
 					return nil
 				}),
 				cli.Allow(cli.MaxArgs(2)),
-			),
+			},
 			wantErr: false,
 			stdout:  "Hello from maxargs\n",
 		},
 		{
 			name: "maxargs fail",
-			cmd: cli.New(
-				"maxargs",
+			options: []cli.Option{
 				cli.Args([]string{"loads", "of", "args", "here", "wow", "so", "many"}),
 				cli.Run(func(cmd *cli.Command, args []string) error {
 					fmt.Fprintln(cmd.Stdout(), "Hello from maxargs")
 					return nil
 				}),
 				cli.Allow(cli.MaxArgs(3)),
-			),
+			},
 			wantErr: true,
-			errMsg:  "command maxargs has a limit of 3 arguments, but got 7: [loads of args here wow so many]",
+			errMsg:  "command test has a limit of 3 arguments, but got 7: [loads of args here wow so many]",
 		},
 		{
 			name: "exactargs pass",
-			cmd: cli.New(
-				"exactargs",
+			options: []cli.Option{
 				cli.Args([]string{"two", "args"}),
 				cli.Run(func(cmd *cli.Command, args []string) error {
 					fmt.Fprintln(cmd.Stdout(), "Hello from exactargs")
 					return nil
 				}),
 				cli.Allow(cli.ExactArgs(2)),
-			),
+			},
 			wantErr: false,
 			stdout:  "Hello from exactargs\n",
 		},
 		{
 			name: "exactargs fail",
-			cmd: cli.New(
-				"exactargs",
+			options: []cli.Option{
 				cli.Args([]string{"not", "three", "but", "four"}),
 				cli.Run(func(cmd *cli.Command, args []string) error {
 					fmt.Fprintln(cmd.Stdout(), "Hello from exactargs")
 					return nil
 				}),
 				cli.Allow(cli.ExactArgs(3)),
-			),
+			},
 			wantErr: true,
-			errMsg:  "command exactargs requires exactly 3 arguments, but got 4: [not three but four]",
+			errMsg:  "command test requires exactly 3 arguments, but got 4: [not three but four]",
 		},
 		{
 			name: "betweenargs pass",
-			cmd: cli.New(
-				"betweenargs",
+			options: []cli.Option{
 				cli.Args([]string{"two", "args"}),
 				cli.Run(func(cmd *cli.Command, args []string) error {
 					fmt.Fprintln(cmd.Stdout(), "Hello from betweenargs")
 					return nil
 				}),
 				cli.Allow(cli.BetweenArgs(1, 4)),
-			),
+			},
 			wantErr: false,
 			stdout:  "Hello from betweenargs\n",
 		},
 		{
 			name: "betweenargs fail high",
-			cmd: cli.New(
-				"betweenargs",
+			options: []cli.Option{
 				cli.Args([]string{"not", "three", "but", "more", "than", "four"}),
 				cli.Run(func(cmd *cli.Command, args []string) error {
 					fmt.Fprintln(cmd.Stdout(), "Hello from betweenargs")
 					return nil
 				}),
 				cli.Allow(cli.BetweenArgs(1, 4)),
-			),
+			},
 			wantErr: true,
-			errMsg:  "command betweenargs requires between 1 and 4 arguments, but got 6: [not three but more than four]",
+			errMsg:  "command test requires between 1 and 4 arguments, but got 6: [not three but more than four]",
 		},
 		{
 			name: "betweenargs fail low",
-			cmd: cli.New(
-				"betweenargs",
+			options: []cli.Option{
 				cli.Args([]string{"not", "three"}),
 				cli.Run(func(cmd *cli.Command, args []string) error {
 					fmt.Fprintln(cmd.Stdout(), "Hello from betweenargs")
 					return nil
 				}),
 				cli.Allow(cli.BetweenArgs(3, 5)),
-			),
+			},
 			wantErr: true,
-			errMsg:  "command betweenargs requires between 3 and 5 arguments, but got 2: [not three]",
+			errMsg:  "command test requires between 3 and 5 arguments, but got 2: [not three]",
 		},
 		{
 			name: "validargs pass",
-			cmd: cli.New(
-				"validargs",
+			options: []cli.Option{
 				cli.Args([]string{"valid", "args", "only"}),
 				cli.Run(func(cmd *cli.Command, args []string) error {
 					fmt.Fprintln(cmd.Stdout(), "Hello from validargs")
 					return nil
 				}),
-				cli.Allow(
-					cli.ValidArgs([]string{"only", "valid", "args"}),
-				), // Order doesn't matter
-			),
+				cli.Allow(cli.ValidArgs([]string{"only", "valid", "args"})), // Order doesn't matter
+			},
+
 			wantErr: false,
 			stdout:  "Hello from validargs\n",
 		},
 		{
 			name: "validargs fail",
-			cmd: cli.New(
-				"validargs",
+			options: []cli.Option{
 				cli.Args([]string{"valid", "args", "only", "bad"}),
 				cli.Run(func(cmd *cli.Command, args []string) error {
 					fmt.Fprintln(cmd.Stdout(), "Hello from validargs")
 					return nil
 				}),
 				cli.Allow(cli.ValidArgs([]string{"only", "valid", "args"})),
-			),
+			},
 			wantErr: true,
-			errMsg:  "command validargs got an invalid argument bad, expected one of [only valid args]",
+			errMsg:  "command test got an invalid argument bad, expected one of [only valid args]",
 		},
 		{
 			name: "combine pass",
-			cmd: cli.New(
-				"combine",
+			options: []cli.Option{
 				cli.Args([]string{"four", "args", "all", "valid"}),
 				cli.Run(func(cmd *cli.Command, args []string) error {
 					fmt.Fprintln(cmd.Stdout(), "Hello from combine")
@@ -231,14 +216,13 @@ func TestArgValidators(t *testing.T) {
 						cli.ValidArgs([]string{"valid", "all", "four", "args"}),
 					),
 				),
-			),
+			},
 			wantErr: false,
 			stdout:  "Hello from combine\n",
 		},
 		{
 			name: "combine fail",
-			cmd: cli.New(
-				"combine",
+			options: []cli.Option{
 				cli.Args([]string{"valid", "args", "only", "bad", "five"}),
 				cli.Run(func(cmd *cli.Command, args []string) error {
 					fmt.Fprintln(cmd.Stdout(), "Hello from combine")
@@ -250,9 +234,9 @@ func TestArgValidators(t *testing.T) {
 						cli.ValidArgs([]string{"only", "valid", "args", "here"}),
 					),
 				),
-			),
+			},
 			wantErr: true,
-			errMsg:  "command combine requires between 1 and 4 arguments, but got 5: [valid args only bad five]",
+			errMsg:  "command test requires between 1 and 4 arguments, but got 5: [valid args only bad five]",
 		},
 	}
 
@@ -261,10 +245,12 @@ func TestArgValidators(t *testing.T) {
 			stderr := &bytes.Buffer{}
 			stdout := &bytes.Buffer{}
 
-			cli.Stderr(stderr)(tt.cmd)
-			cli.Stdout(stdout)(tt.cmd)
+			// Test specific overrides to the options in the table
+			options := []cli.Option{cli.Stdout(stdout), cli.Stderr(stderr)}
 
-			err := tt.cmd.Execute()
+			cmd := cli.New("test", slices.Concat(tt.options, options)...)
+
+			err := cmd.Execute()
 			test.WantErr(t, err, tt.wantErr)
 
 			if tt.wantErr {
