d02b642de2d27e303779efa77ab6698afe5d1065
Tom Fleet <me@followtheprocess.codes>
2024-07-27 17:11:32 +0100

    Better docs on the options (#34)
    * Better docs on the options
    
    * Update TODO

--- option.go
+++ option.go
@@ -77,6 +77,12 @@ func (c *config) build() *Command {
 }
 
 // Stdin is an [Option] that sets the Stdin for a [Command].
+//
+// Successive calls will simply overwrite any previous calls. Without this option
+// the command will default to [os.Stdin].
+//
+//	// Set stdin to os.Stdin (the default anyway)
+//	cli.New("test", cli.Stdin(os.Stdin))
 func Stdin(stdin io.Reader) Option {
 	f := func(cfg *config) error {
 		cfg.stdin = stdin
@@ -86,6 +92,13 @@ func Stdin(stdin io.Reader) Option {
 }
 
 // Stdout is an [Option] that sets the Stdout for a [Command].
+//
+// Successive calls will simply overwrite any previous calls. Without this option
+// the command will default to [os.Stdout].
+//
+//	// Set stdout to a temporary buffer
+//	buf := &bytes.Buffer{}
+//	cli.New("test", cli.Stdout(buf))
 func Stdout(stdout io.Writer) Option {
 	f := func(cfg *config) error {
 		cfg.stdout = stdout
@@ -95,6 +108,13 @@ func Stdout(stdout io.Writer) Option {
 }
 
 // Stderr is an [Option] that sets the Stderr for a [Command].
+//
+// Successive calls will simply overwrite any previous calls. Without this option
+// the command will default to [os.Stderr].
+//
+//	// Set stderr to a temporary buffer
+//	buf := &bytes.Buffer{}
+//	cli.New("test", cli.Stderr(buf))
 func Stderr(stderr io.Writer) Option {
 	f := func(cfg *config) error {
 		cfg.stderr = stderr
@@ -104,6 +124,13 @@ func Stderr(stderr io.Writer) Option {
 }
 
 // Short is an [Option] that sets the one line usage summary for a [Command].
+//
+// The one line usage will appear in the help text as well as alongside
+// subcommands when they are listed.
+//
+// Successive calls will simply overwrite any previous calls.
+//
+//	cli.New("rm", cli.Short("Delete files and directories"))
 func Short(short string) Option {
 	f := func(cfg *config) error {
 		cfg.short = short
@@ -113,6 +140,12 @@ func Short(short string) Option {
 }
 
 // Long is an [Option] that sets the full description for a [Command].
+//
+// The long description will appear in the help text for a command.
+//
+// Successive calls will simply overwrite any previous calls.
+//
+//	cli.New("rm", cli.Long("... lots of text here"))
 func Long(long string) Option {
 	f := func(cfg *config) error {
 		cfg.long = long
@@ -137,8 +170,7 @@ func Long(long string) Option {
 //	# Delete a folder recursively without confirmation
 //	$ myrm ./dir --recursive --force
 //
-// An arbitrary number of examples can be added to a [Command], each call to Example
-// will add another example.
+// An arbitrary number of examples can be added to a [Command], and calls to [Example] are additive.
 func Example(comment, command string) Option {
 	f := func(cfg *config) error {
 		cfg.examples = append(cfg.examples, example{comment: comment, command: command})
@@ -151,6 +183,8 @@ func Example(comment, command string) Option {
 //
 // The run function is the actual implementation of your command i.e. what you
 // want it to do when invoked.
+//
+// Successive calls overwrite previous ones.
 func Run(run func(cmd *Command, args []string) error) Option {
 	f := func(cfg *config) error {
 		cfg.run = run
@@ -160,6 +194,14 @@ func Run(run func(cmd *Command, args []string) error) Option {
 }
 
 // Args is an [Option] that sets the arguments for a [Command].
+//
+// Without this option, the command will default to os.Args[1:], this option is particularly
+// useful for testing.
+//
+// Successive calls override previous ones.
+//
+//	// Override arguments for testing
+//	cli.New("test", cli.Args([]string{"test", "me"}))
 func Args(args []string) Option {
 	f := func(cfg *config) error {
 		cfg.args = args
@@ -169,6 +211,10 @@ func Args(args []string) Option {
 }
 
 // Version is an [Option] that sets the version for a [Command].
+//
+// Without this option, the command defaults to a version of "dev".
+//
+//	cli.New("test", cli.Version("v1.2.3"))
 func Version(version string) Option {
 	f := func(cfg *config) error {
 		cfg.version = version
@@ -181,6 +227,9 @@ func Version(version string) Option {
 //
 // A [Command] will have a default implementation of this function that prints a default
 // format of the version info to [os.Stderr].
+//
+// This option is particularly useful if you want to inject ldflags in at build time for
+// e.g commit hash.
 func VersionFunc(fn func(cmd *Command) error) Option {
 	f := func(cfg *config) error {
 		cfg.versionFunc = fn
@@ -191,6 +240,9 @@ func VersionFunc(fn func(cmd *Command) error) Option {
 
 // SubCommands is an [Option] that attaches 1 or more subcommands to the command being configured.
 func SubCommands(subcommands ...*Command) Option {
+	// TODO: We need to handle some potential misconfigurations here
+	// 1) The user could attempt to add the command itself as a subcommand
+	// 2) The user could try and add a duplicate subcommand, it shouldn't appear in the slice twice
 	f := func(cfg *config) error {
 		// Add the subcommands to the command this is being called on
 		cfg.subcommands = append(cfg.subcommands, subcommands...)
@@ -203,6 +255,11 @@ func SubCommands(subcommands ...*Command) Option {
 //
 // You provide a validator function that returns an error if it encounters invalid arguments, and it will
 // be run for you, passing in the non-flag arguments to the [Command] that was called.
+//
+// Successive calls overwrite previous ones.
+//
+//	// No positional arguments allowed
+//	cli.New("test", cli.Allow(cli.NoArgs))
 func Allow(validator func(cmd *Command, args []string) error) Option {
 	f := func(cfg *config) error {
 		cfg.allowArgs = validator
@@ -211,8 +268,18 @@ func Allow(validator func(cmd *Command, args []string) error) Option {
 	return option(f)
 }
 
-// Flag is an [Option] that adds a flag to a [Command].
+// Flag is an [Option] that adds a flag to a [Command], storing its value in a variable.
+//
+// The variable is set when the flag is parsed during command execution.
+//
+//	// Add a force flag
+//	var force bool
+//	cli.New("rm", cli.Flag(&force, "force", "f", false, "Force deletion without confirmation"))
 func Flag[T flag.Flaggable](p *T, name string, short string, value T, usage string) Option {
+	// TODO: Some potential errors here although pflag just panics on most of them
+	// when replacing with my own version, I need to handle the errors properly here like
+	// duplicate flags etc.
+
 	f := func(cfg *config) error {
 		flag := flag.New(p, name, short, value, usage)
 		var defVal string
