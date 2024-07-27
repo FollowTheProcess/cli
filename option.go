17bc5e355a010560378118a3c2291763c0ae0fee
Tom Fleet <tom.fleet@chetwood.co>
2024-07-27 16:30:49 +0100

    Refactor functional options to act on an intermediate struct

--- option.go
+++ option.go
@@ -1,48 +1,124 @@
 package cli
 
 import (
+	"fmt"
 	"io"
 
 	"github.com/FollowTheProcess/cli/internal/flag"
 	"github.com/spf13/pflag"
 )
 
+// TODO: We now have the ability to handle config errors in options so we should validate where possible
+
 // Option is a functional option for configuring a [Command].
-type Option func(*Command)
+type Option interface {
+	// Apply the option to the config
+	apply(*config) error
+}
+
+// option is a function adapter implementing the Option interface, analogous
+// to http.HandlerFunc.
+type option func(*config) error
+
+// apply implements the Option interface for option.
+func (o option) apply(cfg *config) error {
+	return o(cfg)
+}
+
+// config represents the internal configuratio of a [Command].
+type config struct {
+	stdin       io.Reader
+	stdout      io.Writer
+	stderr      io.Writer
+	run         func(cmd *Command, args []string) error
+	flags       *pflag.FlagSet
+	versionFunc func(cmd *Command) error
+	parent      *Command
+	allowArgs   func(cmd *Command, args []string) error
+	name        string
+	short       string
+	long        string
+	version     string
+	examples    []example
+	args        []string
+	subcommands []*Command
+}
+
+// build builds an returns a Command from the config.
+func (c config) build() *Command {
+	cmd := &Command{
+		stdin:       c.stdin,
+		stdout:      c.stdout,
+		stderr:      c.stderr,
+		run:         c.run,
+		flags:       c.flags,
+		versionFunc: c.versionFunc,
+		parent:      c.parent,
+		allowArgs:   c.allowArgs,
+		name:        c.name,
+		short:       c.short,
+		long:        c.long,
+		version:     c.version,
+		examples:    c.examples,
+		args:        c.args,
+		subcommands: c.subcommands,
+	}
+
+	// Loop through each subcommand and set this command as their immediate parent
+	for _, subcommand := range cmd.subcommands {
+		subcommand.parent = cmd
+	}
+
+	// Add the help and version flags
+	cmd.flagSet().BoolP("help", "h", false, fmt.Sprintf("Show help for %s", cmd.name))
+	cmd.flagSet().BoolP("version", "v", false, fmt.Sprintf("Show version info for %s", cmd.name))
+
+	return cmd
+}
 
 // Stdin is an [Option] that sets the Stdin for a [Command].
 func Stdin(stdin io.Reader) Option {
-	return func(cmd *Command) {
-		cmd.stdin = stdin
+	f := func(cfg *config) error {
+		cfg.stdin = stdin
+		return nil
 	}
+	return option(f)
 }
 
 // Stdout is an [Option] that sets the Stdout for a [Command].
 func Stdout(stdout io.Writer) Option {
-	return func(cmd *Command) {
-		cmd.stdout = stdout
+	f := func(cfg *config) error {
+		cfg.stdout = stdout
+		return nil
 	}
+	return option(f)
 }
 
 // Stderr is an [Option] that sets the Stderr for a [Command].
 func Stderr(stderr io.Writer) Option {
-	return func(cmd *Command) {
-		cmd.stderr = stderr
+	f := func(cfg *config) error {
+		cfg.stderr = stderr
+		return nil
 	}
+	return option(f)
 }
 
 // Short is an [Option] that sets the one line usage summary for a [Command].
 func Short(short string) Option {
-	return func(cmd *Command) {
-		cmd.short = short
+	f := func(cfg *config) error {
+		cfg.short = short
+		return nil
 	}
+	return option(f)
 }
 
 // Long is an [Option] that sets the full description for a [Command].
 func Long(long string) Option {
-	return func(cmd *Command) {
-		cmd.long = long
+	f := func(cfg *config) error {
+		cfg.long = long
+		return nil
 	}
+	return option(f)
 }
 
 // Example is an [Option] that adds an example to a [Command].
@@ -64,9 +140,11 @@ func Long(long string) Option {
 // An arbitrary number of examples can be added to a [Command], each call to Example
 // will add another example.
 func Example(comment, command string) Option {
-	return func(cmd *Command) {
-		cmd.examples = append(cmd.examples, example{comment: comment, command: command})
+	f := func(cfg *config) error {
+		cfg.examples = append(cfg.examples, example{comment: comment, command: command})
+		return nil
 	}
+	return option(f)
 }
 
 // Run is an [Option] that sets the run function for a [Command].
@@ -74,23 +152,29 @@ func Example(comment, command string) Option {
 // The run function is the actual implementation of your command i.e. what you
 // want it to do when invoked.
 func Run(run func(cmd *Command, args []string) error) Option {
-	return func(cmd *Command) {
-		cmd.run = run
+	f := func(cfg *config) error {
+		cfg.run = run
+		return nil
 	}
+	return option(f)
 }
 
 // Args is an [Option] that sets the arguments for a [Command].
 func Args(args []string) Option {
-	return func(cmd *Command) {
-		cmd.args = args
+	f := func(cfg *config) error {
+		cfg.args = args
+		return nil
 	}
+	return option(f)
 }
 
 // Version is an [Option] that sets the version for a [Command].
 func Version(version string) Option {
-	return func(cmd *Command) {
-		cmd.version = version
+	f := func(cfg *config) error {
+		cfg.version = version
+		return nil
 	}
+	return option(f)
 }
 
 // VersionFunc is an [Option] that allows for a custom implementation of the -v/--version flag.
@@ -98,22 +182,21 @@ func Version(version string) Option {
 // A [Command] will have a default implementation of this function that prints a default
 // format of the version info to [os.Stderr].
 func VersionFunc(fn func(cmd *Command) error) Option {
-	return func(cmd *Command) {
-		cmd.versionFunc = fn
+	f := func(cfg *config) error {
+		cfg.versionFunc = fn
+		return nil
 	}
+	return option(f)
 }
 
 // SubCommands is an [Option] that attaches 1 or more subcommands to the command being configured.
 func SubCommands(subcommands ...*Command) Option {
-	return func(cmd *Command) {
+	f := func(cfg *config) error {
 		// Add the subcommands to the command this is being called on
-		cmd.subcommands = append(cmd.subcommands, subcommands...)
-
-		// Loop through each subcommand and set this command as their immediate parent
-		for _, subcommand := range subcommands {
-			subcommand.parent = cmd
-		}
+		cfg.subcommands = append(cfg.subcommands, subcommands...)
+		return nil
 	}
+	return option(f)
 }
 
 // Allow is an [Option] that allows for validating positional arguments to a [Command].
@@ -121,20 +204,22 @@ func SubCommands(subcommands ...*Command) Option {
 // You provide a validator function that returns an error if it encounters invalid arguments, and it will
 // be run for you, passing in the non-flag arguments to the [Command] that was called.
 func Allow(validator func(cmd *Command, args []string) error) Option {
-	return func(cmd *Command) {
-		cmd.allowArgs = validator
+	f := func(cfg *config) error {
+		cfg.allowArgs = validator
+		return nil
 	}
+	return option(f)
 }
 
 // Flag is an [Option] that adds a flag to a [Command].
 func Flag[T flag.Flaggable](p *T, name string, short string, value T, usage string) Option {
-	return func(cmd *Command) {
+	f := func(cfg *config) error {
 		flag := flag.New(p, name, short, value, usage)
 		var defVal string
 		if flag.Type() == "bool" {
 			defVal = "true"
 		}
-		cmd.flagSet().AddFlag(&pflag.Flag{
+		cfg.flags.AddFlag(&pflag.Flag{
 			Name:        name,
 			Shorthand:   short,
 			Usage:       usage,
@@ -142,5 +227,7 @@ func Flag[T flag.Flaggable](p *T, name string, short string, value T, usage stri
 			DefValue:    flag.String(),
 			NoOptDefVal: defVal,
 		})
+		return nil
 	}
+	return option(f)
 }
