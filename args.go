678713d453ff21b7a78c0d7109f7eea37bd4c2e2
Tom Fleet <me@followtheprocess.codes>
2024-07-27 19:40:24 +0100

    Add option validation now we can handle errors properly (#37)
    * Add some basic option validation
    
    * Test the multi error logic
    
    * Add some more
    
    * Tidy up ArgValidators

--- args.go
+++ args.go
@@ -15,17 +15,21 @@ type ArgValidator func(cmd *Command, args []string) error
 // it never returns an error.
 //
 // This is the default argument validator on a [Command] instantiated with cli.New.
-func AnyArgs(cmd *Command, args []string) error {
-	return nil
+func AnyArgs() ArgValidator {
+	return func(cmd *Command, args []string) error {
+		return nil
+	}
 }
 
 // NoArgs is a positional argument validator that does not allow any arguments,
 // it returns an error if there are any arguments.
-func NoArgs(cmd *Command, args []string) error {
-	if len(args) > 0 {
-		return fmt.Errorf("command %s accepts no arguments but got %v", cmd.name, args)
+func NoArgs() ArgValidator {
+	return func(cmd *Command, args []string) error {
+		if len(args) > 0 {
+			return fmt.Errorf("command %s accepts no arguments but got %v", cmd.name, args)
+		}
+		return nil
 	}
-	return nil
 }
 
 // MinArgs is a positional argument validator that requires at least n arguments.
