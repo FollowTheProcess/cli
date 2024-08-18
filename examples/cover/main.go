8523878059d853248f5d30ea2fe1980b8240a853
Tom Fleet <me@followtheprocess.codes>
2024-08-18 08:33:41 +0100

    Generate the cover image from a file (#84)
    * Generate the cover image from a file
    
    * Tweak filepaths
    
    * Tweak Taskfile

---
+++ examples/cover/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/FollowTheProcess/cli"
+)
+
+func main() {
+	var count int
+	cmd, err := cli.New(
+		"demo",
+		cli.Short("Short description of your command"),
+		cli.Long("Much longer text..."),
+		cli.Version("v1.2.3"),
+		cli.Allow(cli.MinArgs(1)),
+		cli.Stdout(os.Stdout),
+		cli.Example("Do a thing", "demo thing --count"),
+		cli.Flag(&count, "count", 'c', 0, "Count the thing"),
+		cli.Run(func(cmd *cli.Command, args []string) error {
+			fmt.Fprintf(cmd.Stdout(), "Hello from demo")
+			return nil
+		}),
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := cmd.Execute(); err != nil {
+		log.Fatalln(err)
+	}
+}
